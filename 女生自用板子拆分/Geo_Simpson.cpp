9ee6382976cb1e6e922d422edceb98aeac7628d8
ATRI <A@T.RI>
2021-06-11 09:07:11 +0800

    fix: 重命名

--- 女生自用板子拆分/Geo_Simpson.cpp
+++ 女生自用板子拆分/Geo_Simpson.cpp
@@ -12,17 +12,17 @@ namespace Geometry
         {
             return (F(l) + 4 * F((l + r) / 2) + F(r)) * (r - l) / 6;
         }
-        inline FLOAT_ asr(FLOAT_ l, FLOAT_ r, FLOAT_ eps, FLOAT_ ans)
+        inline FLOAT_ adaptive(FLOAT_ l, FLOAT_ r, FLOAT_ eps, FLOAT_ ans)
         {
             FLOAT_ mid = (l + r) / 2;
             FLOAT_ _l = simpson(l, mid), _r = simpson(mid, r);
             if (abs(_l + _r - ans) <= 15 * eps)
                 return (_l + _r - ans) / 15 + _l + _r;
-            return asr(l, mid, eps / 2, _l) + asr(mid, r, eps / 2, _r);
+            return adaptive(l, mid, eps / 2, _l) + adaptive(mid, r, eps / 2, _r);
         }
-        FLOAT_ asr(FLOAT_ l, FLOAT_ r, FLOAT_ eps)
+        FLOAT_ adaptive(FLOAT_ l, FLOAT_ r, FLOAT_ eps)
         {
-            return asr(l, r, eps, simpson(l, r));
+            return adaptive(l, r, eps, simpson(l, r));
         }
     };
 
