bffe2863ed74f4c3975b190c0cb5fff1217baf93
Yaya <114.514@191.9>
2021-07-24 10:32:58 +0800

    add: rangell和range分开

--- 女生自用板子拆分/foreach.cpp
+++ 女生自用板子拆分/foreach.cpp
@@ -1,6 +1,6 @@
 #include "Headers.cpp"
 
-struct range
+struct rangell
 {
     struct _iterator
     {
@@ -19,9 +19,35 @@ struct range
     };
     _iterator _finish;
     _iterator _begin;
-    range(LL to) : _begin(0), _finish(to) {}
-    range(LL from, LL to) : _begin(from), _finish(to) {}
-    range(LL from, LL to, LL step) : _begin(from, step), _finish(to, step) {}
+    rangell(LL to) : _begin(0), _finish(to) {}
+    rangell(LL from, LL to) : _begin(from), _finish(to) {}
+    rangell(LL from, LL to, LL step) : _begin(from, step), _finish(to, step) {}
+    _iterator &begin() { return _begin; }
+    _iterator &end() { return _finish; }
+};
+
+struct range
+{
+    struct _iterator
+    {
+        int _start;
+        int _step;
+        _iterator(int from, int step) : _start(from), _step(step) {}
+        _iterator(int from) : _start(from), _step(1) {}
+        inline bool sign(int x) { return x < 0; }
+        bool operator!=(_iterator &b) { return _start != b._start and sign(b._start - _start) == sign(_step); }
+        int operator*() { return _start; }
+        _iterator &operator++()
+        {
+            _start += _step;
+            return *this;
+        }
+    };
+    _iterator _finish;
+    _iterator _begin;
+    range(int to) : _begin(0), _finish(to) {}
+    range(int from, int to) : _begin(from), _finish(to) {}
+    range(int from, int to, int step) : _begin(from, step), _finish(to, step) {}
     _iterator &begin() { return _begin; }
     _iterator &end() { return _finish; }
 };
