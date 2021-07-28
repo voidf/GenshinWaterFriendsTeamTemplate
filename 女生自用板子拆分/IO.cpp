3231a04515eac490e16d5e0507bf70d6ea1fd660
Yaya <114.514@191.9>
2021-07-28 15:08:48 +0800

    improve: isdigit看汇编似乎更快

--- 女生自用板子拆分/IO.cpp
+++ 女生自用板子拆分/IO.cpp
@@ -1,9 +1,7 @@
-
+#include "Headers.cpp"
 
 // char buf[1<<23],*p1=buf,*p2=buf,obuf[1<<23],*O=obuf; // 或者用fread更难调的快读
 // #define getchar() (p1==p2&&(p2=(p1=buf)+fread(buf,1,1<<21,stdin),p1==p2)?EOF:*p1++)
-
-
 
 template <class T>
 void print(T x)
@@ -21,36 +19,38 @@ void print(T x)
 }
 // fwrite(obuf,O-obuf,1,stdout);
 
-
-
 template <class T>
 inline void qr(T &n)
 {
     n = 0;
     int c = getchar();
     if (c == EOF)
-    {
         throw "End of file!";
-    }
     bool sgn = 0;
 
-    while (c > '9' || c < '0')
+    while (!isdigit(c))
     {
         if (c == '-')
             sgn ^= 1;
         c = getchar();
         if (c == EOF)
-        {
             throw "End of file!";
-        }
     }
 
-    while (c <= '9' && c >= '0')
+    while (isdigit(c))
     {
-        n = (n << 3) + (n << 1) + (c ^ 0x30);
+        n = (n * 10) + (c ^ 0x30);
         c = getchar();
     }
 
     if (sgn)
         n = -n;
+}
+
+inline char qrc()
+{
+    char c;
+    while (!islower(c = getchar()))
+        ;
+    return c;
 }
