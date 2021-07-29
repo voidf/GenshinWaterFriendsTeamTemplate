37fa09d87203b52d11f8acebfbdfd796fa18662f
Kaguya <kaguya@Kaguya.cn>
2021-07-29 11:09:51 +0800

    add: 某科学的扩展指令

--- 女生自用板子拆分/Headers.cpp
+++ 女生自用板子拆分/Headers.cpp
@@ -145,3 +145,30 @@ int clz(unsigned long long N)
 
 int log2int(int x) { return 31 - __builtin_clz(x); }
 int log2ll(long long x) { return 63 - __builtin_clzll(x); }
+
+// 扩栈
+
+
+extern int main2(void) __asm__ ("_main2");
+ 
+int main2() {
+    char test[255 << 20];
+    memset(test, 42, sizeof(test));
+    printf(":)\n");
+    exit(0); // 得使用exit0退出，不能使用return 0
+}
+ 
+int main() {
+    int size = 256 << 20;  // 256Mb
+    char *p = (char *)malloc(size) + size;
+    __asm__ __volatile__(
+        "movl  %0, %%esp\n"
+        "pushl $_exit\n" 
+        "jmp _main2\n"
+        :: "r"(p));
+    // __asm__ __volatile__(
+    //     "movq  %0, %%rsp\n"
+    //     "pushq $exit\n" 
+    //     "jmp main2\n"
+    //     :: "r"(p));
+}
