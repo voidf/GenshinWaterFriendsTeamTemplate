04aa4d6226f6487d3c4efdf2809e3ec3eea36df3
Yaya <114.514@191.9>
2021-08-13 11:00:44 +0800

    加入判精度的intereps

--- 女生自用板子拆分/Geo_Base.cpp
+++ 女生自用板子拆分/Geo_Base.cpp
@@ -11,6 +11,15 @@ namespace Geometry
     const double smooth_const2 = 0.479999989271164; // 二次项平滑系数
     const double smooth_const3 = 0.234999999403954; // 三次项平滑系数
 
+    int intereps(FLOAT_ x)
+    {
+        if (x < -decimal_round)
+            return -1;
+        else if (x > decimal_round)
+            return 1;
+        return 0;
+    }
+
     const FLOAT_ PI = acos(-1);
     bool round_compare(FLOAT_ a, FLOAT_ b) { return round(DEC * a) == round(DEC * b); }
     FLOAT_ Round(FLOAT_ a) { return round(DEC * a) / DEC; }
