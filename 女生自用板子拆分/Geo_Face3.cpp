1c792c7c2d6e612e68ff8035c8c7885ffb1641a7
ATRI <A@T.RI>
2021-12-15 13:00:25 +0800

    add: 根据法向构造坐标系面

--- 女生自用板子拆分/Geo_Face3.cpp
+++ 女生自用板子拆分/Geo_Face3.cpp
@@ -6,8 +6,24 @@
 
 namespace Geometry
 {
-    struct Face3 : std::array<Vector3, 3>
+	struct Face3 : std::array<Vector3, 3>
 	{
+		inline void constructCoordinate(const Vector3 &z, Vector3 &x, Vector3 &y)
+		{
+			if (std::abs(z.x) > std::abs(z.y))
+				y = Vector3(z.z, 0, -z.x);
+			else
+				y = Vector3(0, z, z, -z, y);
+			x = Vector3::Cross(y, z);
+		}
+		Face3(const Vector3 &normal, const Vector3 &offset)
+		{
+			Vector3 x, y;
+			constructCoordinate(normal, x, y);
+			(*this)[0] = offset;
+			(*this)[1] = offset + x;
+			(*this)[2] = offset + y;
+		}
 		Face3(const Vector3 &v0, const Vector3 &v1, const Vector3 &v2) : std::array<Vector3, 3>({v0, v1, v2}) {}
 		template <typename... Args>
 		Face3(bool super, Args &&...args) : std::array<Vector3, 3>(std::forward<Args>(args)...) {}
@@ -21,7 +37,7 @@ namespace Geometry
 		/* 点到平面代数距离，一次sqrt */
 		inline FLOAT_ distanceS(const Vector3 &p) const { return Vector3::Dot(p - at(0), normal().normalized()); }
 		/* 点到平面的投影，无损 */
-		inline Vector3 project(const Vector3 &p) const{return p - normal() * Vector3::Dot(p - at(0), normal()) / normal().sqrMagnitude();}
+		inline Vector3 project(const Vector3 &p) const { return p - normal() * Vector3::Dot(p - at(0), normal()) / normal().sqrMagnitude(); }
 	};
 }
 
